5afc75fc376a48277cd9fd6626a077d84e7bd84c
Stanimir Hristov <stanimir.hristov@buildit.bg>
2024-10-09 17:17:22 +0300

    Initial Commit

---
+++ ClearPoolWater/ClearPoolWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearPoolWaterApp.swift
+//  ClearPoolWater
+//
+//  Created by Stanimir Hristov on 9.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ClearPoolWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClearPoolWaterTests/ClearPoolWaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClearPoolWaterTests.swift
+//  ClearPoolWaterTests
+//
+//  Created by Stanimir Hristov on 9.10.24.
+//
+
+import Testing
+@testable import ClearPoolWater
+
+struct ClearPoolWaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

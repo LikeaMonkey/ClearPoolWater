4df096eb3e83efa98e6f8e528a40fa3985372ed4
Stanimir Hristov <darmenter@outlook.com>
2024-11-14 20:06:56 +0200

    Add AccountViewModelTests

---
+++ ClearPoolWaterTests/Tests/ViewModelTests/AccountViewModelTests.swift
@@ -0,0 +1,34 @@
+//
+//  AccountViewModelTests.swift
+//  ClearPoolWater
+//
+//  Created by Stanimir Hristov on 14.11.24.
+//
+
+import Testing
+
+@testable import ClearPoolWater
+
+struct AccountViewModelTests {
+    @Test
+    func testFetchSuccess() async throws {
+        let user = User(id: 1, email: "test@test.com", role: .user)
+        let model = AccountViewModel(apiClient: MockAPIManager(result: user))
+
+        await model.fetchAccount()
+
+        #expect(model.state == .success)
+        #expect(model.user == user)
+    }
+
+    @Test
+    func testFetchFail() async throws {
+        let error = APIError.invalidResponse
+        let model = AccountViewModel(apiClient: MockAPIManager(error: error))
+
+        await model.fetchAccount()
+
+        #expect(model.state == .failure)
+        #expect(model.user == nil)
+    }
+}

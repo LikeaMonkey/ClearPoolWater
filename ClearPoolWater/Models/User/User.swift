855d1fddbc49ca66584ed40c0cb2d704f5260b4f
Stanimir Hristov <darmenter@outlook.com>
2024-11-14 20:10:08 +0200

    Make User Equatable

--- ClearPoolWater/Models/User/User.swift
+++ ClearPoolWater/Models/User/User.swift
@@ -5,12 +5,12 @@
 //  Created by Stanimir Hristov on 11.10.24.
 //
 
-struct User: Codable {
+struct User: Codable, Equatable {
     var id: Int
     var email: String
     var role: Role
 
-    enum Role: String, Codable {
+    enum Role: String, Codable, Equatable {
         case admin
         case user
         case paidUser = "paid_user"

d5e93f14766d62753ca3d60e46ca908e52cf8170
Stanimir Hristov <darmenter@outlook.com>
2024-11-26 11:24:48 +0200

    Make ErrorView accept any errorMessage

--- ClearPoolWater/Views/Common/ErrorView.swift
+++ ClearPoolWater/Views/Common/ErrorView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ErrorView: View {
+    let errorMessage: LocalizedStringKey
     let reloadAction: (() -> Void)?
+
+    init(
+        errorMessage: LocalizedStringKey = Constants.defaultErrorMessage,
+        reloadAction: (() -> Void)? = nil
+    ) {
+        self.errorMessage = errorMessage
+        self.reloadAction = reloadAction
+    }
 
     var body: some View {
         VStack(spacing: 12) {
-            Text("Something went wrong. Please check your network connection and try again.")
+            Text(errorMessage)
 
             if let reloadAction {
                 Button("Reload", action: reloadAction)
             }
         }
         .padding()
+    }
+}
+
+extension ErrorView {
+    struct Constants {
+        static let defaultErrorMessage: LocalizedStringKey =
+            "Something went wrong. Please check your network connection and try again."
     }
 }
 

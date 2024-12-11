f484a2e4362aa882073c36d75f4eefc7812f25e5
Stanimir Hristov <darmenter@outlook.com>
2024-12-11 23:36:16 +0200

    Add CustomListStyle ViewModifier

---
+++ ClearPoolWater/Views/ViewModifiers/CustomListStyle.swift
@@ -0,0 +1,24 @@
+//
+//  CustomListStyle.swift
+//  ClearPoolWater
+//
+//  Created by Stanimir Hristov on 11.12.24.
+//
+
+import SwiftUI
+
+struct CustomListStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .listStyle(.insetGrouped)
+            .listSectionSpacing(20)
+            .contentMargins(.top, 12)
+            .scrollContentBackground(.hidden)
+    }
+}
+
+extension View {
+    func customListStyle() -> some View {
+        modifier(CustomListStyle())
+    }
+}

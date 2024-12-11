7245d23cca771622fb7959201d613f333bdbb440
Stanimir Hristov <darmenter@outlook.com>
2024-12-11 23:39:44 +0200

    Add ErrorMessage

---
+++ ClearPoolWater/Models/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  ClearPoolWater
+//
+//  Created by Stanimir Hristov on 26.11.24.
+//
+
+import SwiftUI
+
+struct ErrorMessage {
+    static var defaultMessage: LocalizedStringKey =
+        "Something went wrong. Please check your network connection and try again."
+}

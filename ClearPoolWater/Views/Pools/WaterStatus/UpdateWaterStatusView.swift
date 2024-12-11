9d1628cba56826ab5a3e5a4fbe3e5efdf631447e
Stanimir Hristov <darmenter@outlook.com>
2024-12-12 00:23:09 +0200

    Update UpdateWaterStatusView

--- ClearPoolWater/Views/Pools/WaterStatus/UpdateWaterStatusView.swift
+++ ClearPoolWater/Views/Pools/WaterStatus/UpdateWaterStatusView.swift
@@ -24,43 +24,54 @@ struct UpdateWaterStatusView: View {
                 ProgressView()
                     .frame(alignment: .center)
             } else {
-                VStack(spacing: 15) {
-                    TitledView(title: "pH") {
-                        SliderTextFieldView(value: $viewModel.ph, range: 0...14, step: 0.1)
-                    }
-                    TitledView(title: "Chlorine") {
-                        SliderTextFieldView(value: $viewModel.chlorine, range: 0...15, step: 0.1)
-                    }
-                    TitledView(title: "Alkalinity") {
-                        SliderTextFieldView(value: $viewModel.alkalinity, range: 0...300, step: 10)
-                    }
-                    TitledView(title: "Temperature") {
-                        SliderTextFieldView(value: $viewModel.temperature, range: 0...50, step: 0.5)
-                    }
-                }
-                .padding(.horizontal, 20)
+                waterTraitSliders
             }
         }
         .navigationTitle("Update water status")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Button("Cancel") {
-                    dismiss()
-                }
-            }
-
-            ToolbarItem {
-                Button("Done") {
-                    Task {
-                        await viewModel.updateWaterStatus()
-                        dismiss()
-                    }
-                }
-            }
+            cancelToolbarItem
+            doneToolbarItem
         }
         .task {
             await viewModel.fetchWaterStatus()
+        }
+    }
+
+    private var waterTraitSliders: some View {
+        VStack(spacing: 15) {
+            TitledView(title: "pH") {
+                SliderTextFieldView(value: $viewModel.ph, range: 0...14, step: 0.1)
+            }
+            TitledView(title: "Chlorine") {
+                SliderTextFieldView(value: $viewModel.chlorine, range: 0...15, step: 0.1)
+            }
+            TitledView(title: "Alkalinity") {
+                SliderTextFieldView(value: $viewModel.alkalinity, range: 0...300, step: 10)
+            }
+            TitledView(title: "Temperature") {
+                SliderTextFieldView(value: $viewModel.temperature, range: 0...50, step: 0.5)
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+
+    private var cancelToolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            Button("Cancel") {
+                dismiss()
+            }
+        }
+    }
+
+    private var doneToolbarItem: some ToolbarContent {
+        ToolbarItem {
+            Button("Done") {
+                Task {
+                    await viewModel.updateWaterStatus()
+                    dismiss()
+                }
+            }
         }
     }
 }

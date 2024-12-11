f3484f09d417bbd6bc385dc0fe7ccaf0d7bd16b7
Stanimir Hristov <darmenter@outlook.com>
2024-12-11 23:41:55 +0200

    Update PoolWaterStatusSection

--- ClearPoolWater/Views/Pools/WaterStatus/PoolWaterStatusSection.swift
+++ ClearPoolWater/Views/Pools/WaterStatus/PoolWaterStatusSection.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct PoolWaterStatusSection: View {
     @State private var viewModel: WaterStatusViewModel
 
+    @State private var isConfirmationDialogPresented = false
+    @State private var isWaterStatusSheetPresented = false
+    @State private var isScanViewPresented = false
+
     init(poolId: Int) {
         viewModel = WaterStatusViewModel(poolId: poolId)
     }
@@ -19,52 +23,18 @@ struct PoolWaterStatusSection: View {
             HStack {
                 Text("Water Balance")
                     .font(.headline)
-
                 Spacer()
-
-                UpdateWaterStatusButton(
-                    poolId: viewModel.poolId,
-                    waterStatusId: viewModel.waterStatusId
-                ) {
-                    Task {
-                        await viewModel.fetchWaterStatus()
-                    }
+                Button("Update") {
+                    isConfirmationDialogPresented.toggle()
                 }
+                .disabled(viewModel.waterStatusId == nil)
             }
 
             if !viewModel.isLoaded || viewModel.isLoading {
                 ProgressView()
                     .frame(alignment: .center)
             } else {
-                HStack {
-                    WaterParameterGauge(
-                        name: "pH",
-                        value: viewModel.ph,
-                        range: 0...14,
-                        idealRange: 7.2...7.6
-                    )
-                    Spacer()
-                    WaterParameterGauge(
-                        name: "Cl",
-                        value: viewModel.chlorine,
-                        range: 0...10,
-                        idealRange: 1...1.5
-                    )
-                    Spacer()
-                    WaterParameterGauge(
-                        name: "Alkal",
-                        value: viewModel.alkalinity,
-                        range: 0...300,
-                        idealRange: 80...100
-                    )
-                    Spacer()
-                    WaterParameterGauge(
-                        name: "Temp",
-                        value: viewModel.temperature,
-                        range: 0...50,
-                        idealRange: 24...30
-                    )
-                }
+                waterTraitGauges
             }
         }
         .padding(20)
@@ -72,6 +42,69 @@ struct PoolWaterStatusSection: View {
         .cornerRadius(10)
         .task {
             await viewModel.fetchWaterStatus()
+        }
+        .confirmationDialog(
+            "Update water status",
+            isPresented: $isConfirmationDialogPresented,
+            titleVisibility: .visible
+        ) {
+            Button("Manual") {
+                isWaterStatusSheetPresented.toggle()
+            }
+            Button("Scan") {
+                isScanViewPresented.toggle()
+            }
+        }
+        .sheet(isPresented: $isWaterStatusSheetPresented) {
+            NavigationView {
+                UpdateWaterStatusView(
+                    poolId: viewModel.poolId,
+                    waterStatusId: viewModel.waterStatusId!
+                )
+            }
+            .presentationDetents([.medium])
+            .onDisappear {
+                Task {
+                    await viewModel.fetchWaterStatus()
+                }
+            }
+        }
+        .fullScreenCover(isPresented: $isScanViewPresented) {
+            NavigationStack {
+                ScanView()
+            }
+        }
+    }
+
+    private var waterTraitGauges: some View {
+        HStack {
+            WaterParameterGauge(
+                name: "pH",
+                value: viewModel.ph,
+                range: 0...14,
+                idealRange: 7.2...7.6
+            )
+            Spacer()
+            WaterParameterGauge(
+                name: "Cl",
+                value: viewModel.chlorine,
+                range: 0...10,
+                idealRange: 1...1.5
+            )
+            Spacer()
+            WaterParameterGauge(
+                name: "Alkal",
+                value: viewModel.alkalinity,
+                range: 0...300,
+                idealRange: 80...100
+            )
+            Spacer()
+            WaterParameterGauge(
+                name: "Temp",
+                value: viewModel.temperature,
+                range: 0...50,
+                idealRange: 24...30
+            )
         }
     }
 }

c410b8850a5b04ea7b157852f139bd3da6adfe6d
Stanimir Hristov <darmenter@outlook.com>
2024-12-15 15:50:02 +0200

    Make CoreBluetooth preconcurency

--- ClearPoolWater/Models/Monitoring/UUIDs.swift
+++ ClearPoolWater/Models/Monitoring/UUIDs.swift
@@ -5,9 +5,9 @@
 //  Created by Stanimir Hristov on 8.12.24.
 //
 
-import CoreBluetooth
+@preconcurrency import CoreBluetooth
 
 enum UUIDs {
     static let sensorService = CBUUID(string: "d888a9c2-f3cc-11ed-a05b-0242ac120003")
-    static let temperatureCharacteristic = CBUUID(string: "d888a9c3-f3cc-11ed-a05b-0242ac120003")
+    static let phCharacteristic = CBUUID(string: "d888a9c3-f3cc-11ed-a05b-0242ac120003")
 }

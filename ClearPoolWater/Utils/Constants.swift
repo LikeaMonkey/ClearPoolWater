2faa4e28289be868147d13a5abb40619d7aa2105
Stanimir Hristov <darmenter@outlook.com>
2024-12-26 22:10:26 +0200

    Add Contant for cornerRadius

---
+++ ClearPoolWater/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ClearPoolWater
+//
+//  Created by Stanimir Hristov on 21.12.24.
+//
+import Foundation
+
+struct Constants {
+    static let cornerRadius: CGFloat = 12
+}
